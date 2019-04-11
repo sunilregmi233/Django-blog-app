1acd6020bb38b56513933b9c9ca8ee44feb48dea
sunilregmi233 <sunilregmi223@gmail.com>
2019-04-11 20:42:31 +0545

    admin user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'blog',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
