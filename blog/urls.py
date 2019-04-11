71fb677efb65c8be21e2c124d548bf943720e0ac
sunilregmi233 <sunilregmi223@gmail.com>
2019-04-11 19:52:35 +0545

    update blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogPostView, BlogDetailView, BlogCreateView
+from .views import BlogPostView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 
 urlpatterns = [
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogPostView.as_view(), name='home')
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 # Create your views here.
 class BlogPostView(ListView):
@@ -16,3 +16,7 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
